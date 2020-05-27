16bcb31a02eb7478a0551dd1391b8002a774416e
Sukchan Lee <acetcom@gmail.com>
2020-05-26 20:34:46 -0400

    update it

--- lib/sbi/openapi/model/inline_response_200.c
+++ lib/sbi/openapi/model/inline_response_200.c
@@ -25,7 +25,9 @@ void OpenAPI_inline_response_200_free(OpenAPI_inline_response_200_t *inline_resp
     OpenAPI_lnode_t *node;
     OpenAPI_list_for_each(inline_response_200->_links, node) {
         OpenAPI_map_t *localKeyValue = (OpenAPI_map_t*)node->data;
-        OpenAPI_links_value_schema_free(localKeyValue->value);
+        /*
+           OpenAPI_links_value_schema_free(localKeyValue->value);
+         */
         ogs_free(localKeyValue);
     }
     OpenAPI_list_free(inline_response_200->_links);
